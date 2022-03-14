132e0867ed7e256bb884ea2d1576e0a299b9a02a
cjwe <christajwest@gmail.com>
2022-03-14 14:06:38 +0100

    Add jsdoc comments

--- auth.js
+++ auth.js
@@ -1,26 +1,35 @@
-const jwtSecret = "your_jwt_secret";
+const jwtSecret = 'your_jwt_secret';
 
-const jwt = require("jsonwebtoken"),
-  passport = require("passport");
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
 
-require("./passport");
+require('./passport');
 
+/**
+ * Generates JWT web token.
+ * @function generateJWTToken
+ * @param {object} - Object containing the user data
+ * @returns {string} - jwt web token
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username,
-    expiresIn: "7d",
-    algorithm: "HS256",
+    expiresIn: '7d',
+    algorithm: 'HS256',
   });
 };
 
-// POST login
+/**
+ * POST login credentials and create JWT token.
+ * @param {*} router
+ */
 module.exports = (router) => {
   router.use(passport.initialize());
-  router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: "Error authenticating login.",
+          message: 'Error authenticating login.',
           user: user,
         });
       }
